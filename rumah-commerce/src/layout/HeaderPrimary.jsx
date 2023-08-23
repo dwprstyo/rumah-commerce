936c84c67870baf9279e422d280f5fee694afbc5
juangsyah.putra@bni.co.id <kuatdwiprasetiyo@gmail.com>
2023-08-23 21:15:55 +0700

    finish header-primary

--- rumah-commerce/src/layout/HeaderPrimary.jsx
+++ rumah-commerce/src/layout/HeaderPrimary.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+
+import "../css/header-primary.css";
+import Logo from "../assets/rumah.svg";
+import Button from "react-bootstrap/esm/Button";
 
 export const HeaderPrimary = () => {
   return (
-    <div>HeaderPrimary</div>
-  )
-}
+    <div className="p-5 pt-2 pb-2 header-primary d-grid align-items-center">
+      <div className="row">
+        <div className="col d-flex justify-content-start align-items-center gap-4">
+          <img
+            src={Logo}
+            alt="Company Logo"
+            loading="lazy"
+            className="gap-2"
+            width={200}
+          />
+          <div className="hp-text">Dijual</div>
+          <div className="hp-text">Disewa</div>
+          <div className="hp-text">Properti Baru</div>
+          <div className="hp-text">KPR</div>
+          <div className="hp-text">Panduan</div>
+        </div>
+        <div className="col d-flex justify-content-end align-items-center gap-5">
+          <Button variant="light" className="button-white">Iklankan Properti</Button>
+          <div className="hp-text" width={100}>Akun</div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- rumah-commerce/src/pages/Home.jsx
+++ rumah-commerce/src/pages/Home.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
+
+import Carousel from 'react-bootstrap/Carousel';
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
